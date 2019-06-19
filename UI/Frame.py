df5a184feb665a6314efd3c5234f9c688ce3ff8f
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-19 09:23:55 +0300

    frame_completion and creating differents button locations

--- UI/Frame.py
+++ UI/Frame.py
@@ -10,6 +10,24 @@ def main():
     frame = ttk.Frame(root)
     frame.pack()
     frame.config(height=200,width=200)
+
+    #adding buttons to a frame
+    ttk.Button(frame,text="Button 1").grid(row=0,column=0)
+    ttk.Button(frame, text="Button 2").grid(row=0, column=2)
+    ttk.Button(frame, text="Button 3").grid(row=0, column=3)
+
+    #frame 2
+    frame2 = ttk.Frame(root)
+    frame2.pack()
+    frame2.config(height=300,width=300)
+
+    #add buttons to frame 2
+    ttk.Button(frame2,text="Button X").grid(row=0,column=0)
+    ttk.Button(frame2,text="Button Y").grid(row=0,column=1)
+    ttk.Button(frame2, text="Button Z").grid(row=0, column=2)
+    
+    #label frame
+    ttk.LabelFrame(height=200,width=200,text="Third text frame").pack()
     root.mainloop()
 
 
