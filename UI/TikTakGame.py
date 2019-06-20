0705357256fea05cbe027e168ccbffc8aad41dfd
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-20 10:33:32 +0300

    creating buttons and game setup

---
+++ UI/TikTakGame.py
@@ -0,0 +1,34 @@
+#python tikTak toy game
+
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+
+
+def btnClick(id):
+    print("ID: {} ".format(id))
+
+
+def main():
+
+    root = Tk()
+    root.title("Python Game : Player 1")
+
+    #add button
+    btn1 = ttk.Button(root,text='')
+    btn1.grid(row=0,column=0,sticky='snew',ipadx='40',ipady='40')
+    btn1.config(command=lambda :btnClick(1))
+
+
+
+
+
+
+
+
+
+
+    root.mainloop()
+
+
+if __name__ == '__main__':main()
